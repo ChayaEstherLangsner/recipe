51537a06b5ac0051c3fe79c91fc67a59d298b860
chayaestherlangsner <celangsner@gmail.com>
2023-07-05 20:39:03 -0400

    22

--- Database Files/sprocs/RecipeDelete.sql
+++ Database Files/sprocs/RecipeDelete.sql
@@ -1,14 +1,23 @@
 create or alter procedure dbo.RecipeDelete
 (
-	@RecipeID int
+	@RecipeID int,
+	@Message varchar (500) = '' output
 )
 as 
 begin
+declare @return int = 0
+
+	if exists(select * from recipe r where r.RecipeID = @RecipeID and( r.RecipeStatus = 'Published' or (r.RecipeStatus = 'Archived' and  datediff(DAY,r.RecipeDateArchived, getdate()) < 30)))
+	begin 
+	select @return = 1, @Message = 'Cannot delete recipe that is currenly published or archived for less than 30 days'
+	goto finished 
+	end
+	select * from recipe
 	begin try
 		begin tran
-delete RecipeIngredient where RecipeID = @RecipeID
-delete RecipeInstruction where RecipeID = @RecipeID
-delete Recipe where RecipeID = @RecipeID
+	delete RecipeIngredient where RecipeID = @RecipeID
+	delete RecipeInstruction where RecipeID = @RecipeID
+	delete Recipe where RecipeID = @RecipeID
 		commit
 	end try
 	begin catch 
@@ -16,6 +25,7 @@ delete Recipe where RecipeID = @RecipeID
 		throw
 	end catch
 
-
+	finished:
+	return @return
 end 
 go 
