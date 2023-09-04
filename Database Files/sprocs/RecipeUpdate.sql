5b859e26175d05ea75e471a8fa05a43cfee1fba3
Ashira Friedman <77624864+AshiraFriedman@users.noreply.github.com>
2023-09-04 10:31:09 +0300

    comment

--- Database Files/sprocs/RecipeUpdate.sql
+++ Database Files/sprocs/RecipeUpdate.sql
@@ -1,3 +1,4 @@
+--AF Nice job!  See how I formatted the indentation, indenting makes the sproc much easier to read and easier to understand the structure with the 'if else' statement
 create or alter proc dbo.RecipeUpdate
 (
 @RecipeID int  output,
@@ -6,26 +7,24 @@ create or alter proc dbo.RecipeUpdate
 @RecipeName varchar(50),
 @RecipeCalories int
 )
- as
- begin 
-
- select @RecipeID = isnull(@RecipeID, 0)
-if (@RecipeID = 0)
-begin
- insert Recipe(RecipeName,UsersID , CuisineID, RecipeCalories)
- values (@RecipeName, @UsersID, @CuisineID, @RecipeCalories)
-select @RecipeID = SCOPE_IDENTITY()
- end
- else 
- begin
-update Recipe 
-set 
-RecipeName = @RecipeName,
-UsersID = @UsersID, 
-CuisineID = @CuisineID, 
-RecipeCalories = @RecipeCalories
-where RecipeID = @RecipeID
-end
-
- end 
- go
+as
+begin 
+    select @RecipeID = isnull(@RecipeID, 0)
+    if (@RecipeID = 0)
+    begin
+        insert Recipe(RecipeName,UsersID , CuisineID, RecipeCalories)
+        values (@RecipeName, @UsersID, @CuisineID, @RecipeCalories)
+        select @RecipeID = SCOPE_IDENTITY()
+    end
+    else 
+    begin
+        update Recipe 
+        set 
+        RecipeName = @RecipeName,
+        UsersID = @UsersID, 
+        CuisineID = @CuisineID, 
+        RecipeCalories = @RecipeCalories
+    where RecipeID = @RecipeID
+    end
+end 
+go
