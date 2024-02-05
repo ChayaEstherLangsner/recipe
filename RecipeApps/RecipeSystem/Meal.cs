c64b6481cb45ac1c61582717ceb6fb9ef3cfb30b
chayaestherlangsner <celangsner@gmail.com>
2024-02-05 01:26:19 -0500

    #52

--- RecipeApps/RecipeSystem/Meal.cs
+++ RecipeApps/RecipeSystem/Meal.cs
@@ -1,13 +1,4 @@
-﻿using CPUFramework;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeSystem
+﻿namespace RecipeSystem
 {
     public class Meal
     {
--- RecipeApps/RecipeSystem/RecipeStatus.cs
+++ RecipeApps/RecipeSystem/RecipeStatus.cs
@@ -1,12 +1,4 @@
-﻿using CPUFramework;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeSystem
+﻿namespace RecipeSystem
 {
     public class RecipeStatus
     {
